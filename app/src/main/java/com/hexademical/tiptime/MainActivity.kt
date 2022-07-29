b062c4b795e7b7cd905e97f56819f1f893c92751
idontknowtoobrother <idontknowbrotherpls@gmail.com>
2022-07-29 22:14:01 +0700

    optimize code

--- app/src/main/java/com/hexademical/tiptime/MainActivity.kt
+++ app/src/main/java/com/hexademical/tiptime/MainActivity.kt
@@ -1,13 +1,12 @@
 package com.hexademical.tiptime
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.hexademical.tiptime.databinding.ActivityMainBinding
-import java.lang.Math.ceil
 import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -17,28 +16,27 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun displayTip(tip: Double){
+        val formatTip = NumberFormat.getCurrencyInstance().format(Double)
+        binding.tipAmount.text = getString(R.string.tip_amount, formatTip)
+    }
+
     private fun calculateTip() {
         val cost = binding.costOfService.text.toString().toDoubleOrNull()
-        if(cost == null){
-            binding.tipAmount.text = ""
+        if(cost == null || cost == 0.0){
+            displayTip(0.0)
             return
         }
-        val selectId = binding.tipOptions.checkedRadioButtonId
-        val tipPercentage = when(selectId){
+        val tipPercentage = when(binding.tipOptions.checkedRadioButtonId){
             R.id.tip_15_percent -> 0.15
             R.id.tip_18_percent -> 0.18
             else -> 0.20
         }
 
-        val isRoundUp = binding.roundupService.isChecked
-        val tipAmount = when(isRoundUp){
-            true -> ceil(cost * tipPercentage)
+        val tipAmount = when(binding.roundupService.isChecked){
+            true -> kotlin.math.ceil(cost * tipPercentage)
             else -> cost * tipPercentage
         }
-
-        val formatTip = NumberFormat.getCurrencyInstance().format(tipAmount)
-        binding.tipAmount.text = getString(R.string.tip_amount, formatTip)
-
-
+        displayTip(tipAmount)
     }
 }
