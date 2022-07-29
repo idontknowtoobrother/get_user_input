fa916599e5c7d6db67718b1489a842943f6ebc00
idontknowtoobrother <idontknowbrotherpls@gmail.com>
2022-07-29 21:52:02 +0700

    initial: late initial cause forgot sry teacher

---
+++ app/src/main/java/com/hexademical/tiptime/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.hexademical.tiptime
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hexademical.tiptime.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.calculateTip.setOnClickListener {
+            calculateTip()
+        }
+    }
+
+    private fun calculateTip() {
+        val cost = binding.costOfService.text.toString().toDouble()
+
+    }
+}
